3dcff6ed7d6135c43e572f2d5d34314b01378271
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-03-31 15:48:16 +0200

    Updates

--- utils/pages/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function({ createPage, sections }) {
-  return {
-    createHomePage: require('./createHomePage')({ createPage, sections }),
-    createSectionPages: require('./createSectionPages')({ createPage, sections }),
-    createTopicPages: require('./createTopicPages')({ createPage, sections }),
-    createTypePages: require('./createTypePages')({ createPage, sections }),
-  };
-};
--- utils/resources/index.js
+++
@@ -1,8 +0,0 @@
-const config = require('repills-config');
-const typesConfig = config.types;
-
-export function normalizeResource({ node }) {
-  node.frontmatter.color = typesConfig[node.frontmatter.type.join('_')].color;
-  node.frontmatter.typeLabel = typesConfig[node.frontmatter.type.join('_')].label.singular;
-  return node.frontmatter;
-}
