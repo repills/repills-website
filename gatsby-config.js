125359c440080f83624f4636f9d275e9db0c23ab
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-04-01 13:20:39 +0200

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,24 @@ module.exports = {
         path: `${__dirname}/src/resources`,
         name: 'resources'
       },
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
     }
   ]
 };
