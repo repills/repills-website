6c648c9b17275fc357cf84a4f5f959e577de6d29
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-04-05 23:40:53 +0200

    Implements webfontloader

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+import React from 'react';
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
@@ -5,3 +6,36 @@
  */
 
  // You can delete this file if you're not using it
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  return setHeadComponents([
+    <link
+      key={`webfontsloader-preload`}
+      rel="preload"
+      href="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+      as="script"
+    />
+  ]);
+};
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  return setPostBodyComponents([
+    <script
+      key={`webfontsloader-setup`}
+      dangerouslySetInnerHTML={{
+        __html: `
+        WebFontConfig = {
+          google: {
+      families: ["Poppins:400,600"]
+    }
+   };
+   (function(d) {
+      var wf = d.createElement('script'), s = d.scripts[0];
+      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+      wf.async = true;
+      s.parentNode.insertBefore(wf, s);
+   })(document);`
+      }}
+    />
+  ]);
+};
