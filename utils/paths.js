ef0acea3d3ba68894632620f693235d3984eb5eb
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-04-20 00:06:18 +0200

    Fixes paths

--- utils/paths.js
+++ utils/paths.js
@@ -1,15 +1,15 @@
 module.exports = {
   getResourcePagePath: ({slug, publishedAt}) => {
     const _publishedAtDate = publishedAt ? new Date(publishedAt) : null;
-    return (_publishedAtDate && _publishedAtDate.getFullYear() !== 1970) ? `/r/${_publishedAtDate.getFullYear()}/${slug}` : `/r/${slug}`
+    return (_publishedAtDate && _publishedAtDate.getFullYear() !== 1970) ? `/r/${_publishedAtDate.getFullYear()}/${slug}/` : `/r/${slug}/`
   },
   getLastAddedPagePath: index => {
-    return (index && index !== 1) ? `/last-added/${index}` : `/last-added`;
+    return (index && index !== 1) ? `/last-added/${index}/` : `/last-added/`;
   },
   getTopicPagePath: ({index, basePath}) => {
-    return (index && index !== 1) ? `${basePath}/${index}` : basePath;
+    return (index && index !== 1) ? `${basePath}/${index}/` : `${basePath}/`;
   },
   getTypePagePath: ({index, basePath}) => {
-    return (index && index !== 1) ? `${basePath}/${index}` : basePath;
+    return (index && index !== 1) ? `${basePath}/${index}/` : `${basePath}/`;
   }
 };
